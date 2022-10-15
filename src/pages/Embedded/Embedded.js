87feb0a1430041f76c45631dc881ede4f189e8a6
Terje <terjeslaatto@gmail.com>
2022-10-15 22:01:05 +0200

    list adjusting

--- src/pages/Embedded/Embedded.js
+++ src/pages/Embedded/Embedded.js
@@ -6,6 +6,7 @@ const Embedded = () => {
     <>
       <div className="embedded-container">
         <h3>Visning av to testfiler for å vise embedded version </h3>
+        <h4>NB! KREVER LOGIN</h4>
         <div>
           <iframe
             src="https://egms-my.sharepoint.com/personal/heli011_egms_no/_layouts/15/embed.aspx?UniqueId=c47c0f8a-3308-47ce-8a5f-a313a684ebeb"
