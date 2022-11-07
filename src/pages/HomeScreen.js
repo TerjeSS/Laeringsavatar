48d8a52c0c5f13b0b6bbd4cb8a1620cad206b872
Terje <terjeslaatto@gmail.com>
2022-11-07 21:11:00 +0100

    prepare for storage references, and added two new visualizations

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { storageRef } from "../resources/firebase";
 import { listAll, ref } from "firebase/storage";
 import { storage } from "../resources/firebase";
@@ -9,25 +9,15 @@ const HomeScreen = () => {
 
   const animationRef = ref(storageRef, "animations");
 
-  const fileReferences = [];
-  listAll(animationRef)
-    .then((res) => {
-      console.log(res.items);
-      res.items.forEach((itemRef) => {
-        // All the items under listRef.
-        fileReferences.push(res.items);
-      });
-    })
-    .catch((error) => {
-      // Uh-oh, an error occurred!
-    });
+  const [fileReferences, setFileReferences] = useState([]);
 
   return (
     <div>
       <div>Velkommen til 3D Visualisering</div>
       {fileReferences.map((element) => {
-        return <div>{element}</div>;
+        return <div>ello</div>;
       })}
+      <div>after</div>
     </div>
   );
 };
