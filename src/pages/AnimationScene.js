df8af4b75a98210deb7c3cb17886deddbbda4240
Terje <terjeslaatto@gmail.com>
2022-11-07 23:01:55 +0100

    reduced camera render distance

--- src/pages/AnimationScene.js
+++ src/pages/AnimationScene.js
@@ -101,7 +101,7 @@ const AnimationScene = ({ animationFilename }) => {
       75,
       window.innerWidth / window.innerHeight,
       1,
-      1000
+      200
     );
     camera.position.set(0, 2, -4);
 
