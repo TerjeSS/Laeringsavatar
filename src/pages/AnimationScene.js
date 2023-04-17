ba420fed65dce0251378b26db073af4377b09c0b
henlie-oslomet <henlie@oslomet.no>
2023-04-17 14:47:35 +0200

    Updated Three js code

--- src/pages/AnimationScene.js
+++ src/pages/AnimationScene.js
@@ -44,6 +44,19 @@ const AnimationScene = () => {
   }, 1);
 
   //THREE.JS SECTION ***************
+  function loadScene(scene) {
+    const loader = new GLTFLoader();
+    loader.load('/Stage.glb', (gltf) => {
+      gltf.scene.traverse( function( node ) {
+        if ( node.isMesh ) { 
+          node.receiveShadow = true; 
+        }
+      } );
+
+      scene.add(gltf.scene);
+    });	
+  }
+
   function createScene() {
     let mixer;
 
@@ -57,87 +70,39 @@ const AnimationScene = () => {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.shadowMap.enabled = true;
 
     // container.appendChild(renderer.domElement);
     const pmremGenerator = new THREE.PMREMGenerator(renderer);
 
     const scene = new THREE.Scene();
-    const gridHelper = new THREE.GridHelper(25, 25);
-    // scene.add(gridHelper);
-
-    //Walls
-
-    //backwall
-    const geometry = new THREE.PlaneGeometry(35, 25);
-    const material = new THREE.MeshToonMaterial({
-      color: 0xb8bff1,
-      side: THREE.DoubleSide,
-    });
-    const plane = new THREE.Mesh(geometry, material);
-    scene.add(plane);
-    plane.position.set(1, 1, 7);
-
-    //Wwall nr 2
-    const geometry2 = new THREE.PlaneGeometry(30, 25);
-    const material2 = new THREE.MeshBasicMaterial({
-      color: 0x133b5f,
-      side: THREE.DoubleSide,
-    });
-    const plane2 = new THREE.Mesh(geometry2, material2);
-    scene.add(plane2);
-    plane2.position.set(10, 1, 1);
-    plane2.rotateY(1.5707);
-
-    //Wall nr 3
-    const geometry3 = new THREE.PlaneGeometry(30, 25);
-    const plane3 = new THREE.Mesh(geometry3, material2);
-    scene.add(plane3);
-    plane3.position.set(-10, 1, 1);
-    plane3.rotateY(1.5707);
-
-    //Ground
-    const mesh = new THREE.Mesh(
-      new THREE.PlaneGeometry(30, 30),
-      new THREE.MeshPhongMaterial({ color: 0x7d7b7a, depthWrite: false })
-    );
-    mesh.rotation.x = -Math.PI / 2;
-    mesh.receiveShadow = true;
-    scene.add(mesh);
-
-    // Background and environment
-    scene.background = new THREE.Color(0xf0f0f0);
-    scene.environment = pmremGenerator.fromScene(
-      new RoomEnvironment(),
-      0.1
-    ).texture;
-
-    //Light
-
-    // const hemiLight = new THREE.HemisphereLight(0x0a0a0a, 0xffffff);
-    // hemiLight.position.set(2, 60, 2);
-    // const hemihelper = new THREE.HemisphereLightHelper(hemiLight);
-    // scene.add(hemiLight);
-    // scene.add(hemihelper);
-
-    const dirLight = new THREE.DirectionalLight(0x133b5f);
-    const dirLight2 = new THREE.DirectionalLight(0x133b5f);
-    const dirLightHelper = new THREE.DirectionalLightHelper(dirLight);
-
-    scene.add(dirLightHelper);
-    dirLight2.position.set(-10, 7, -10);
-    scene.add(dirLight2);
-    dirLight.position.set(10, 7, -10);
-
-    scene.add(dirLight);
+    scene.background = new THREE.Color( 0x000000 );
 
     //Camera
     const camera = new THREE.PerspectiveCamera(
-      75,
+      50,
       window.innerWidth / window.innerHeight,
-      1,
-      200
+      0.01,
+      60
     );
-    camera.position.set(1, 3, -4);
+    camera.position.set(0, 2, -8);
+    camera.lookAt(0,3,5);
+
+    loadScene(scene);
+
+    // light
+    const dirLight = new THREE.DirectionalLight( 0xeeeeee );
+	  dirLight.position.set( 0, 1, -0.4 );
+	  dirLight.castShadow = false;
+	  scene.add( dirLight );
+
+	  const lightFromBack = new THREE.DirectionalLight( 0x444444 );
+	  lightFromBack.position.set( 0, 1, 0.8 );
+	  lightFromBack.castShadow = true;
+	  scene.add( lightFromBack );
+
+	  const ambLight = new THREE.AmbientLight(0xeeeeee);
+	  scene.add(ambLight);
 
     //Controls
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -156,8 +121,6 @@ const AnimationScene = () => {
       firebaseURL,
       function (gltf) {
         const model = gltf.scene;
-        model.position.set(1, 0, 1);
-        model.scale.set(1.5, 1.5, 1.5);
         scene.add(model);
         model.traverse(function (object) {
           if (object.isMesh) object.castShadow = true;
@@ -166,7 +129,7 @@ const AnimationScene = () => {
         mixer = new THREE.AnimationMixer(model);
 
         //Starts the animation
-        mixer.clipAction(gltf.animations[0]).play();
+        mixer.clipAction(gltf.animations[1]).play();
         const animationDuration = mixer.clipAction(gltf.animations[0])._clip
           .duration;
         animate();
@@ -175,21 +138,21 @@ const AnimationScene = () => {
         document
           .querySelector(".pause-button")
           .addEventListener("click", () => {
-            mixer.clipAction(gltf.animations[0]).paused = true;
+            mixer.clipAction(gltf.animations[1]).paused = true;
           });
         document
           .querySelector(".resume-button")
           .addEventListener("click", () => {
-            mixer.clipAction(gltf.animations[0]).paused = false;
+            mixer.clipAction(gltf.animations[1]).paused = false;
           });
         document
           .querySelector(".reset-button")
           .addEventListener("click", () => {
-            mixer.clipAction(gltf.animations[0]).reset();
+            mixer.clipAction(gltf.animations[1]).reset();
           });
         document.getElementById("speed").addEventListener("change", (e) => {
           mixer
-            .clipAction(gltf.animations[0])
+            .clipAction(gltf.animations[1])
             .setDuration(animationDuration / e.target.value);
         });
       },
