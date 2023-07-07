eda0140784a8a6269a368a2050bee72a6ac3941a
henlie-oslomet <henlie@oslomet.no>
2023-07-07 17:31:02 +0200

    Added picking

--- src/pages/AnimationScene.js
+++ src/pages/AnimationScene.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { RoomEnvironment } from "three/addons/environments/RoomEnvironment.js";
 
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { getDownloadURL, listAll, ref } from "firebase/storage";
 
-import { AnimationAction, AnimationMixer, GridHelper } from "three";
 import ControlPanel from "../components/ControlPanel/ControlPanel";
 import { storage } from "../resources/firebase";
 import { useParams } from "react-router-dom";
+
 
 const AnimationScene = () => {
   const [firebaseURL, setFirebaseURL] = useState("");
@@ -18,7 +17,8 @@ const AnimationScene = () => {
   const storageRef = ref(storage);
   const animationRef = ref(storageRef, "animations");
   const { filename } = useParams();
-  let camera, sceneBB, sceneRef;
+  let camera, sceneBB, controls, bones, bonePos, targetGeometry, model;
+  let selectingCenter = false;
 
   const fetchRefereces = async () => {
     listAll(animationRef)
@@ -55,13 +55,13 @@ const AnimationScene = () => {
       });
 
       sceneBB = new THREE.Box3().setFromObject(gltf.scene);
-      sceneRef = gltf.scene; 
       scene.add(gltf.scene);
     });
   }
 
   function createScene() {
     let mixer;
+    bonePos = new THREE.Vector3();
 
     const clock = new THREE.Clock();
     const container = document.querySelector(".canvas");
@@ -72,12 +72,9 @@ const AnimationScene = () => {
     });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.outputColorSpace = THREE.SRGBColorSpace;
     renderer.shadowMap.enabled = true;
     renderer.physicallyCorrectLights = true;
-
-    // container.appendChild(renderer.domElement);
-    const pmremGenerator = new THREE.PMREMGenerator(renderer);
 
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0x000000);
@@ -92,28 +89,32 @@ const AnimationScene = () => {
     camera.position.set(0, 2, 3);
     camera.lookAt(0, 2, -5);
 
+    //Controls
+    controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 1, 0);
+    controls.update();
+
     loadScene(scene);
 
     // lights
-    const dirLight = new THREE.DirectionalLight('white', 4);
+    const dirLight = new THREE.DirectionalLight('white');
     dirLight.position.set(0, 1, -0.4);
     dirLight.castShadow = false;
     scene.add(dirLight);
 
-    const lightFromBack = new THREE.DirectionalLight('white', 4);
+    const lightFromBack = new THREE.DirectionalLight('white');
     lightFromBack.position.set(0, 1, 0.8);
     lightFromBack.castShadow = true;
     scene.add(lightFromBack);
 
-    const ambLight = new THREE.AmbientLight(0xeeeeee,2);
+    const ambLight = new THREE.AmbientLight('white',5);
     scene.add(ambLight);
 
-    //Controls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0, 1, 0);
-    controls.update();
-    controls.enablePan = true;
-    controls.enableDamping = true;
+    // Geometry
+    const sphere = new THREE.SphereGeometry(0.1);
+    const targetMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    targetGeometry = new THREE.Mesh(sphere, targetMaterial);
+    scene.add(targetGeometry);
 
     //Loaders
     const dracoLoader = new DRACOLoader();
@@ -124,13 +125,27 @@ const AnimationScene = () => {
     loader.load(
       firebaseURL,
       function (gltf) {
-        const model = gltf.scene;
+        model = gltf.scene;
         scene.add(model);
 
         model.traverse(function (object) {
-          if (object.isMesh) object.castShadow = true;
+          if (object.isMesh) 
+          {
+            object.castShadow = true;
+            object.geometry.boundingBox = sceneBB.clone();
+            object.geometry.boundingBox.expandByScalar(2);
+            const center = new THREE.Vector3();
+            sceneBB.getCenter(center);
+            const radius = sceneBB.getSize(new THREE.Vector3()).length() / 2;
+            object.geometry.boundingSphere = new THREE.Sphere(center, radius);
+            object.geometry.boundingSphere.expandByPoint(new THREE.Vector3(7,7,7));
+          }
         });
-
+        bones = model.children[1].children;
+        bonePos.set(0,0,0);
+        bonePos.applyMatrix4(bones[0].matrix);
+        controls.target.copy(bonePos);
+        
         mixer = new THREE.AnimationMixer(model);
 
         //Starts the animation
@@ -144,16 +159,19 @@ const AnimationScene = () => {
           .querySelector(".pause-button")
           .addEventListener("click", () => {
             mixer.clipAction(gltf.animations[1]).paused = true;
+            selectingCenter = true;
           });
         document
           .querySelector(".resume-button")
           .addEventListener("click", () => {
             mixer.clipAction(gltf.animations[1]).paused = false;
+            selectingCenter = false;
           });
         document
           .querySelector(".reset-button")
           .addEventListener("click", () => {
             mixer.clipAction(gltf.animations[1]).reset();
+            selectingCenter = false;
           });
         document.getElementById("speed").addEventListener("change", (e) => {
           mixer
@@ -174,6 +192,24 @@ const AnimationScene = () => {
 
       renderer.setSize(window.innerWidth / window.innerHeight);
     };
+    container.onclick = function ( event ) {
+      if(selectingCenter)
+      {
+        const pointer = new THREE.Vector2();
+        pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+        const raycaster = new THREE.Raycaster();
+        raycaster.setFromCamera( pointer, camera );
+        const intersects = raycaster.intersectObjects(model.children);
+
+        if(intersects.length > 0){
+            controls.target.copy(intersects[0].point);
+            targetGeometry.position.copy(intersects[0].point);
+        }
+
+      }
+    }
 
     //Recursive animate-function
     function animate() {
@@ -181,8 +217,6 @@ const AnimationScene = () => {
 
       const delta = clock.getDelta();
       mixer.update(delta);
-
-      controls.update();
 
       if(sceneBB)
       {
